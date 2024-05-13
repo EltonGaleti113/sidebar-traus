032095d5c3bc70ee237bb4173c18ace665f23c66
Elton Galeti <egaleti2003@gmail.com>
2024-05-13 11:06:54 -0300

    Added sidebar javascript document

---
+++ app/assets/config/sidebar.js
@@ -0,0 +1,8 @@
+ 
+      
+const closeMenu = document.getElementById('logo-sidebar')
+
+function closeSideBar() {
+//?Criar uma function para quando onclick for ativado, essa function entre em açao
+    closeMenu.innerHTML('')
+}
