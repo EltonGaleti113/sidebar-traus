4b51e65d675f56537a68b7f679bbbe0693cde70d
Elton Galeti <egaleti2003@gmail.com>
2024-05-14 12:58:55 -0300

    new function openSidebar

--- app/assets/config/sidebar.js
+++ app/assets/config/sidebar.js
@@ -1,18 +1,21 @@
- function closeSidebar() {
-    var sidebar = document.getElementById("logo-sidebar");
-    var ariaMenu = sidebar.getAttribute("aria-menu");
-
+function closeSidebar() {
+  var sidebar = document.getElementById("logo-sidebar");
+  var ariaMenu = sidebar.getAttribute("aria-menu");
     if (ariaMenu === "true") {
       sidebar.setAttribute("aria-menu", "false");
-      sidebar.style.width = "65px";
+      sidebar.style.width = "50px";
     } else {
       sidebar.setAttribute("aria-menu", "true");
       sidebar.removeAttribute("style");
-    }
+    } 
 
-    //   if (ariaMenu === "false") {
-      
-    // } else{
-      
+    // if (sidebar.style.width === "50px") {
+    //   onclick.applysidebar.style.transform = "translatex(256)"
     // }
   }
+
+function openSidebarByButton() {
+  var sidebar = document.getElementById("logo-sidebar");
+    
+    sidebar.classList.add("w-63");
+}
